a25cdbf5742e840920508b5f1c69b3ec7ac6b0b0
Martin Gustafsson <gusten86@gmail.com>
2017-12-22 11:58:02 +0100

    feat(all): aurelia implementation

---
+++ src/Presentation/Nop.Web/wwwroot/aurelia/main.ts
@@ -0,0 +1,11 @@
+﻿import { Aurelia, PLATFORM } from 'aurelia-framework';
+
+export async function configure(aurelia: Aurelia) {
+    aurelia.use
+        .defaultBindingLanguage()
+        .defaultResources()
+        .developmentLogging()
+        .globalResources(PLATFORM.moduleName('welcome')); // <- Make the component a global resource
+    await aurelia.start();
+    await aurelia.enhance(document.querySelector('welcome')); // <- Enhence the component
+}
